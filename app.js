550044fe67c606388bdfc7b5744ac9cb9068f347
Gabriela Araujo <gabyara237@gmail.com>
2025-11-06 20:44:54 -0800

    Add solution for exercise 5

--- app.js
+++ app.js
@@ -50,8 +50,24 @@ Exercise 4
 Solve Exercise 4 here:
 */
 
-game.party.push(pokemon.find(pokemonStarter => pokemonStarter.starter === true))
+game.party.push(pokemon.find(pokemonStarter => pokemonStarter.starter === true));
 
 console.log(game);
 
 
+/*
+Exercise 5
+1. Choose three more Pokémon from the `pokemon` array and add them to your party.
+2. Consider different attributes like 'type' or 'HP' for your selection. Which array method will you use to add them?
+
+
+Solve Exercise 5 here:
+*/
+
+const pokemons = pokemon.filter(currentPokemon => currentPokemon.hp>80).slice(0,3)
+
+pokemons.forEach((pokemon)=>{
+    game.party.push(pokemon);
+})
+
+console.log(game.party);
